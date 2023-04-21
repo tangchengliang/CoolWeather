fb92b56884964c53b305b0e65ec88b0db18185a8
Leon <cy.tcl@qq.com>
2023-04-21 13:44:45 +0800

    Initial commit

---
+++ app/src/main/java/com/coolweather/android/db/BackGround.java
@@ -0,0 +1,27 @@
+package com.coolweather.android.db;
+
+public class BackGround {
+    private String name;
+    private int imageId;
+
+    public BackGround(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+}
